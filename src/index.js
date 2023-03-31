5273587ccd695be4bb21eea6b5c045035ee9fa26
MelnychenkoJenny <melnichenkojenny@gmail.com>
2023-03-31 15:05:59 +0300

    pagination + style

--- src/index.js
+++ src/index.js
@@ -1,34 +1,67 @@
-import fetchImages from './js/fetchImages';
+import ImageApiService from './js/fetchImages';
+import LoadMoreBtn from './js/load-more-btn';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const searchNameImgForm = document.querySelector('#search-form');
-const searchNameImgInput = document.querySelector('input[name="searchQuery"]')
-const containerGallery = document.querySelector('.gallery-list')
-const loadMoreBtn = document.querySelector('.load-more')
+const searchNameImgInput = document.querySelector('input[name="searchQuery"]');
+const containerGallery = document.querySelector('.gallery-list');
+
+const imageApiService = new ImageApiService();
+const loadMoreBtn = new LoadMoreBtn({ selector: '.load-more', hidden: true });
 
 searchNameImgForm.addEventListener('submit', onSearchImageClickBtn);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMoreImage);
 
 function onSearchImageClickBtn(e) {
   e.preventDefault();
 
-  
-  const searchNameImg = e.target.searchQuery.value.trim();
-  if(searchNameImg==='') {
+  imageApiService.query = e.target.searchQuery.value.trim();
+  imageApiService.resetPage();
+  if (imageApiService.query === '') {
     Notify.info('Будь ласка, заповніть поле для пошуку зображень');
-    clearMarkUp()
-    loadMoreBtn.classList.add('is-hidden')
+    loadMoreBtn.hide();
+    clearMarkUp();
     return;
   }
-  fetchImages(searchNameImg)
-    .then((data) => {
-        clearMarkUp();
-        if(data.hits.length === 0) {
-            Notify.failure('На жаль, немає зображень, які відповідають вашому пошуковому запиту. Будь ласка спробуйте ще раз :)')
-        }
-        Notify.success(`Ура! Ми знайшли ${data.totalHits} зображень/зображення.`);
-       makeGalleryMarkUp(data);
-       loadMoreBtn.classList.remove('is-hidden')
 
+  loadMoreBtn.show();
+  loadMoreBtn.disable();
+  clearMarkUp();
+  imageApiService
+    .fetchImages()
+    .then(data => {
+      if (data.hits.length === 0) {
+        Notify.failure(
+          'На жаль, немає зображень, які відповідають вашому пошуковому запиту. Будь ласка спробуйте ще раз :)'
+        );
+        loadMoreBtn.hide();
+      } else if (data.hits.length < 12) {
+        Notify.success(`Ура! Ми знайшли ${data.totalHits} зображень/зображення.`);
+        loadMoreBtn.hide();
+      }
+      Notify.success(`Ура! Ми знайшли ${data.totalHits} зображень/зображення.`);
+      containerGallery.insertAdjacentHTML('beforeend', makeGalleryMarkUp(data));
+      loadMoreBtn.enable();
+    })
+    .catch(er => console.log(er));
+}
+
+function onLoadMoreImage() {
+  imageApiService.incrementPage();
+  loadMoreBtn.disable();
+
+  imageApiService
+    .fetchImages()
+    .then(data => {
+      if (Math.ceil(data.totalHits / imageApiService.count) === imageApiService.page) {
+        Notify.failure(
+          'Вибачте, але Ви досягли кінця результатів пошуку.'
+        );
+        loadMoreBtn.hide();
+      }
+      Notify.success(`Ура! Ми знайшли ${data.totalHits - imageApiService.count} зображень/зображення.`);
+      containerGallery.insertAdjacentHTML('beforeend', makeGalleryMarkUp(data));
+      loadMoreBtn.enable();
     })
     .catch(er => console.log(er));
 }
@@ -46,15 +79,13 @@ function makeGalleryMarkUp({ hits }) {
     }) => {
       return `<li class="gallery__item">
     <a href="${largeImageURL}" class="gallery__link link">
-      <img src="${webformatURL}" alt="${tags}" class="gallery__img" width="300">
-    </a>
-    </li>
-    <div class="box-info">
+      <div class="gallery__thumb"><img src="${webformatURL}" alt="${tags}" class="gallery__img" width="300" loading="lazy"></div>
+      <div class="box-info">
     <ul class="gallery-info list">
       <li class="gallery-info__item">
         <p class="gallery-info__text">
-          <b class="gallery-info__value">
-    Лайки:
+          <b>
+    Лайки
         </b>
         ${likes}
       </p>
@@ -62,7 +93,7 @@ function makeGalleryMarkUp({ hits }) {
       <li class="gallery-info__item">
         <p class="gallery-info__text">
           <b class="gallery-info__value">
-    Комментарі:
+    Комментарі
         </b>
         ${comments}
       </p>
@@ -70,7 +101,7 @@ function makeGalleryMarkUp({ hits }) {
       <li class="gallery-info__item">
         <p class="gallery-info__text">
           <b class="gallery-info__value">
-    Перегляди:
+    Перегляди
         </b>
         ${views}
       </p>
@@ -78,19 +109,22 @@ function makeGalleryMarkUp({ hits }) {
       <li class="gallery-info__item">
         <p class="gallery-info__text">
           <b class="gallery-info__value">
-    Завантаження:
+    Завантаження
         </b>
         ${downloads}
       </p>
       </li>
     </ul>
-    </div>`;
+    </div>
+    </a>
+    </li>
+    `;
     }
   );
-  containerGallery.innerHTML = markUp.join('')
+  return markUp.join('');
 }
 
-  function clearMarkUp() {
-    containerGallery.innerHTML = '';
-    searchNameImgInput.value = "";
-  }
+function clearMarkUp() {
+  containerGallery.innerHTML = '';
+  searchNameImgInput.value = '';
+}
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,13 +1,36 @@
 import axios from 'axios';
+
 const API_KEY = '34879645-22f56e50c3160d67803a5d79c';
 const API_URL = 'https://pixabay.com/api/';
 
-export default async function fetchImages(searchName) {
-  const response = await fetch(
-    `${API_URL}?key=${API_KEY}&q=${searchName}&image_type=photo&orientation=horizontal&safesearch=true&lang=pl,bg`
-  );
-  if (!response.ok) {
-    throw new Error(response.status);
+export default class ImageApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.count = 12;
   }
-  return await response.json();
+  async fetchImages() {
+    console.log(this);
+    
+    const response = await fetch(
+      `${API_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&lang=pl,bg&per_page=${this.count}&page=${this.page}`
+    );
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return await response.json();
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
 }
